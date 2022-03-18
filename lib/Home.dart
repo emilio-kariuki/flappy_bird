f623d139bac728df7363338057eff42587eb33f1
emilio <emilio113kariuki@gmail.com>
2022-03-18 17:42:22 +0300

    March 18, 2022 5:42 PM

--- lib/Home.dart
+++ lib/Home.dart
@@ -54,8 +54,9 @@ class _HomeState extends State<Home> {
       height = -4.9 * time * time + 2 * time;
       setState(() {
         birdLocation = initialHeight - height;
-        bXOne -= 0.05;
-        bXTwo -= 0.05;
+      });
+      setState(() {
+        
       });
       if (birdIsDead()) {
         timer.cancel();
